c9c1725e9b89364cb0653f347c5b2a504c2216c5
sravanthiaaa <154407568+sravanthiaaa@users.noreply.github.com>
2024-03-04 18:29:15 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import counter from './counter'
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1> hello world </h1>
+     <counter/>
+    </>
+  )
+}
+
+export default App
